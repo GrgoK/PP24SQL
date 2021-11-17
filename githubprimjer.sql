d3f24304cd1696decb6b73bdfb621f60da2660e0
GrgoK <93468203+GrgoK@users.noreply.github.com>
2021-11-17 18:43:29 +0100

    dodao character set

--- githubprimjer.sql
+++ githubprimjer.sql
@@ -1,8 +1,9 @@
 drop database if exists githubprimjer;
 create database githubprimjer;
-use githubprimjer;
+use githubprimjer character set utf8;
 
 create table git( 
     sifra int not null primary key auto_increment,
     naziv varchar(50)not null
 );
+
